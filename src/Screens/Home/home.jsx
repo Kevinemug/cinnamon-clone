d9c8fafa51af280fdb7b8db9e533ccc86c6c2160
Kevinemug <kevineeuwamahoro@gmail.com>
2023-05-16 18:25:22 +0300

    new changes

--- src/Screens/Home/home.jsx
+++ src/Screens/Home/home.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import HomeNav from "../../components/HomeNav/homeNav";
 import Navigation from "../../components/Navigation/navigation";
 import debounce from "lodash.debounce";
+import {AiOutlineArrowLeft} from 'react-icons/ai'
 import Title from "../../components/Title/title";
 const Home = () => {
   const [isTop, setIsTop] = useState(true);
@@ -29,7 +30,7 @@ const Home = () => {
   return (
     <>
       <HomeNav />
-      <div className="bg-[#1C1C1C] bg-hero-pattern h-[100vh] w-screen">
+      <div className="bg-[#1C1C1C] bg-hero-pattern h-[100vh] w-screen max-w-[100%] ">
         <Title
           color="text-[#f6f6f6]"
           t1="Results focused design &"
@@ -38,7 +39,16 @@ const Home = () => {
           shape your product from scratch. Either way, we’ll get your product
           off the ground and build a momentum for your success"
         />
+
       </div>
+      <div className="transform flex -rotate-[90deg] ml-[180px] font-semibold  h-[50%] -mt-[20px] text-[#cbcbcb] origin-bottom-left ">
+<div className="mx-[8px] animate-bounce"><AiOutlineArrowLeft className="text-xl "/></div>
+<div>SEE OUR WORK</div>
+</div>
+<div className="flex  -mt-[200px] ml-[1140px] -mt-[250px]">
+    <div className="h-[140px] w-[140px] bg-no-repeat  shadow-3xl  bg-contain bg-center bg-hero-circle bg-[black] rounded-[50%]"></div>
+</div>
+
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     extend: {
       backgroundImage: {
         'hero-pattern': "url('https://cinnamon.agency/images/noise.png')",
+        'hero-circle': "url('https://cinnamon.agency/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdeloitte-badge.1a6faa3a.png&w=384&q=100')",
       }
+    },
+    boxShadow: {
+      '3xl': '0 35px 45px -15px #5135ff',
     },
   },
   plugins: [],
