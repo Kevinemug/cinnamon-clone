f5fd7eb975df124b995b00c978a0549c255039f8
Mugisha Kevine <Kevinemug>
2023-10-14 19:25:30 +0200

    new changes

--- src/components/subFooter.jsx
+++ src/components/subFooter.jsx
@@ -7,7 +7,7 @@ const SubFooter = () => {
 
 <div className='flex flex-col items-center  justify-center'>
 <div className={`text-[40px] text-center font-["Mont-Bold"]`} >
-    Have a project in mind?
+    Have a project iin mind?
     Let's work together.
 </div>
 <div>
